25cc7ea9404c91e6f0bc870890f64a8a68ebdc92
Stuart Clark <stu@rtclark.net>
2011-10-01 19:39:12 +1000

    #1296028: Fixed issue with generating .dzi file in Apache.

--- deepzoom.tpl.php
+++ deepzoom.tpl.php
@@ -3,7 +3,7 @@
  * @file
  */
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
+<<?php echo '?' ?>xml version="1.0" encoding="UTF-8"?>
 <Image TileSize="<?php echo $size ?>" Overlap="<?php echo $overlap ?>" Format="<?php echo $format ?>" xmlns="http://schemas.microsoft.com/deepzoom/2008">
   <Size Width="<?php echo $width ?>" Height="<?php echo $height ?>"/>
 </Image>
